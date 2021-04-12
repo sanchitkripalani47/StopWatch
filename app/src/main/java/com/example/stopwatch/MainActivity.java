48208ef394decc8af2d16ee229576c9f85ae0c5b
Sanchit Kripalani <sanchitkripalani47@gmail.com>
2021-04-12 23:31:02 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.stopwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Chronometer;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Chronometer chronometer;
+    private boolean running;
+    private long pauseOffSet;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        chronometer = findViewById(R.id.chronometer);
+        chronometer.setFormat("Time:  %s");
+        chronometer.setBase(SystemClock.elapsedRealtime());
+
+        chronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+                if (SystemClock.elapsedRealtime()-chronometer.getBase() >= 10000){
+                    chronometer.setBase(SystemClock.elapsedRealtime());
+                    Toast.makeText(MainActivity.this, "Bing Bong", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void startChronometer(View view1) {
+        if (!running) {
+            chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffSet);
+            chronometer.start();
+            running = true;
+        }
+    }
+
+    public void pauseChronometer(View view2) {
+        if (running) {
+            chronometer.stop();
+            pauseOffSet = SystemClock.elapsedRealtime() - chronometer.getBase();
+            running = false;
+        }
+    }
+
+    public void resetChronometer(View view3) {
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        pauseOffSet = 0;
+    }
+
+}
